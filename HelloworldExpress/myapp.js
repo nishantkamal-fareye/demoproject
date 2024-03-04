273f5b13ca3dd1ab23ff2f07ac288217a4f955f0
imnishantdevops <157911131+imnishantdevops@users.noreply.github.com>
2024-03-04 19:26:35 +0530

    Create myapp.js

---
+++ HelloworldExpress/myapp.js
@@ -0,0 +1,11 @@
+onst express = require('express');
+const app = express();
+const PORT = 9000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
