6d211f264923b68391d794d2e017cadb14222a75
imnishantdevops <157911131+imnishantdevops@users.noreply.github.com>
2024-03-10 23:41:21 +0530

    Update forecast.js

--- weatherapp/forecast.js
+++ weatherapp/forecast.js
@@ -2,12 +2,14 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const port = 6000;
+const port = 3000;
+
+const apiKey = process.env.API_KEY;
 
 app.get('/weather', async (req, res) => {
     try {
         // Make API call to weather service
-        const apiUrl = 'https://api.weatherapi.com/v1/forecast.json?key=1a7ee52df2f34d61b31171106241003&q=28.52,77.19&days=7';
+        const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=28.52,77.19&days=7`;
         const response = await axios.get(apiUrl);
         res.json(response.data);
     } catch (error) {
