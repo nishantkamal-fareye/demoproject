c0f1bcc5422e5582ed3feb30b09acb2853455ad8
imnishantdevops <157911131+imnishantdevops@users.noreply.github.com>
2024-03-11 09:50:32 +0530

    Update forecast.js

--- weatherapp/forecast.js
+++ weatherapp/forecast.js
@@ -1,23 +1,36 @@
-require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
-const port = 6000;
+const port = 3000;
 
-const apiUrl = process.env.API_URL;
+const API_KEY = process.env.API_KEY;
 
-app.get('/weather', async (req, res) => {
-    try {
-        // Make API call to weather service
-        const response = await axios.get(apiUrl);
-        res.json(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+// Define a route for the root path of the application
+app.get('/', (req, res) => {
+  const address = req.query.address; // Read the address query parameter from the request
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${address}&units=metric&appid=${API_KEY}`;
+  console.log(url)
+
+  // Make an HTTP GET request to the API using axios
+  axios.get(url)
+    .then(response => {
+      const data = response.data;
+      const cityName = data.name;
+      const temperature = data.main.temp;
+      const sunsetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString();
+      const message = `City Name: ${cityName}<br>Temperature: ${temperature}&deg;C<br>Sunset Time: ${sunsetTime}`;
+
+      res.send(`<html><body><div id='container'><h1>${message}</h1></div></body></html>`);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Error occurred while fetching weather data');
+    });
 });
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
