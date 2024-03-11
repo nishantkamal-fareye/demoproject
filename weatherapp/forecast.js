5a4ab41f595b0dcaf2b95eda43cbab7ba4eec857
imnishantdevops <157911131+imnishantdevops@users.noreply.github.com>
2024-03-11 10:36:00 +0530

    Update forecast.js

--- weatherapp/forecast.js
+++ weatherapp/forecast.js
@@ -3,30 +3,39 @@ const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = 7003;
 
 const API_KEY = process.env.API_KEY;
 
 // Define a route for the root path of the application
 app.get('/', (req, res) => {
   const address = req.query.address; // Read the address query parameter from the request
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${address}&units=metric&appid=${API_KEY}`;
-  console.log(url)
+  const url = `http://api.openweathermap.org/data/2.5/forecast?q=${address}&units=metric&appid=${API_KEY}`;
 
   // Make an HTTP GET request to the API using axios
   axios.get(url)
     .then(response => {
       const data = response.data;
-      const cityName = data.name;
-      const temperature = data.main.temp;
-      const sunsetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString();
-      const message = `City Name: ${cityName}<br>Temperature: ${temperature}&deg;C<br>Sunset Time: ${sunsetTime}`;
+      const cityName = data.city.name;
+      const forecastList = data.list;
 
-      res.send(`<html><body><div id='container'><h1>${message}</h1></div></body></html>`);
+      // Extract relevant information from the forecast list
+      const forecastData = forecastList.map(item => {
+        const date = new Date(item.dt * 1000).toLocaleDateString();
+        const temperature = item.main.temp;
+        return { date, temperature };
+      });
+
+      // Construct the response message
+      const message = `<h1>Weather Forecast for ${cityName}</h1>`;
+      const forecastItems = forecastData.map(item => `<p>Date: ${item.date}, Temperature: ${item.temperature}&deg;C</p>`).join('');
+      const htmlResponse = `<html><body><div id='container'>${message}${forecastItems}</div></body></html>`;
+
+      res.send(htmlResponse);
     })
     .catch(error => {
       console.error(error);
-      res.status(500).send('Error occurred while fetching weather data');
+      res.status(500).send('Error occurred while fetching weather forecast data');
     });
 });
 
